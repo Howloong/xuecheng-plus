9c2ec0b43314518cdbb6554975a9c2b0a1c865f1
yuhan.kang@outlook.com <yuhan.kang@outlook.com>
2023-02-18 16:08:09 +0800

    课程查询接口开发

---
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/config/GlobalCorsConfig.java
@@ -0,0 +1,30 @@
+package com.xuecheng.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @ClassName: GlobalCorsConfig
+ * @Package: com.xuecheng.system.config
+ * @Description:
+ * @Datetime: 2023/2/17   21:00
+ * @Author: YuHan.Kang@outlook.com
+ */
+@Configuration
+public class GlobalCorsConfig {
+    @Bean
+    public CorsFilter getCorsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
