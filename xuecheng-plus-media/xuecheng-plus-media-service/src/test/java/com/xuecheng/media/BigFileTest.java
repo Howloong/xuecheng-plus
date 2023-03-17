a9066349ff6b128ab76cc91f2d6409811fd9919c
yuhan.kang@outlook.com <yuhan.kang@outlook.com>
2023-03-17 16:14:09 +0800

    freemarker

---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/BigFileTest.java
@@ -0,0 +1,8 @@
+package com.xuecheng.media;/**
+*   @ClassName:    BigFileTest
+*   @Package:    com.xuecheng.media
+*   @Description:
+*   @Datetime:    2023/3/13   18:31
+*   @Author:   YuHan.Kang@outlook.com
+*/public class BigFileTest {
+}
